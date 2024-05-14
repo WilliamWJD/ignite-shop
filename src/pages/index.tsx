5c5104bb62f87653950527e992a0813deba3ad9c
WilliamWJD <william.jose.wjd@gmail.com>
2024-05-14 10:17:21 -0300

    feat: configurando stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import { styled } from '@/styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  color: '#fff',
+  padding: '10px 20px',
+  borderRadius: 10,
+  border: 0,
+  cursor: 'pointer',
+})
+
 export default function Home() {
-  return <h1>Hello Next</h1>
+  return <Button>Enviar</Button>
 }
