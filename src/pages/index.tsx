064226e51aac533575c58d1241b53030fb6d367b
WilliamWJD <william.jose.wjd@gmail.com>
2024-05-14 11:16:39 -0300

    feat: criando card de produto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
-import { styled } from '@/styles'
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  color: '#fff',
-  padding: '10px 20px',
-  borderRadius: 10,
-  border: 0,
-  cursor: 'pointer',
-})
+import camiseta1 from '../assets/1.png'
+import camiseta2 from '../assets/2.png'
+import camiseta3 from '../assets/3.png'
+import camiseta4 from '../assets/4.png'
 
 export default function Home() {
-  return <Button>Enviar</Button>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
