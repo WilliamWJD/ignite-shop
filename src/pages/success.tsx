cca7d6d2e982e43820f6fc9c98a6fb939e30e77e
WilliamWJD <william.jose.wjd@gmail.com>
2024-06-05 21:21:31 -0300

    feat: criando pagina de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,20 @@
+import { SuccessContainer, ImageContainer } from '@/styles/pages/success'
+import Link from 'next/link'
+
 export default function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>William Dias</strong>, sua
+        <strong>Camiseta Beyond the limits</strong> já está a caminho da sua
+        casa
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
